5030323a2c3a6e92a92d941d5ed7ae44102bfabc
Arthur Fauquenot <arthurfauquenot@yahoo.com>
2019-07-26 12:44:38 +0200

    Modify 404 error message

--- generators/app/templates/src/routes/index.js
+++ generators/app/templates/src/routes/index.js
@@ -12,9 +12,7 @@ router.get('/', (req, res) => res.redirect('/docs'));
  * 404 error handling
  */
 router.use((req, res, next) => {
-  const err = new NotFound('This route does not exist.');
-
-  next(err);
+  next(new NotFound(`The requested route '${req.method} ${req.url}' does not exist.`));
 });
 
 module.exports = router;
